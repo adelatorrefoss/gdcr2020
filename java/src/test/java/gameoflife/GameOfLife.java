9a91c71ff38b9e499236a1c1b6e3a0e3a011049d
Antonio de la Torre <antonio@codesai.com>
2020-11-07 13:11:04 +0100

    grid_empty_remains_empty

--- java/src/test/java/gameoflife/GameOfLife.java
+++ java/src/test/java/gameoflife/GameOfLife.java
@@ -2,13 +2,32 @@ package gameoflife;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class GameOfLife {
 
+	private boolean grid[];
+
 	@Test
-	public void fix_me_and_rename() {
-		assertFalse(true);
+	public void grid_empty_remains_empty() {
+		initGrid();
+
+		grid = nextTurn(grid);
+		assertFalse(grid[0]);
+		assertFalse(grid[1]);
+		assertFalse(grid[2]);
+	}
+
+	private void initGrid() {
+		grid =  new boolean[3];
+		grid[0] = false;
+		grid[1] = false;
+		grid[2] = false;
+	}
+
+	private boolean[] nextTurn(boolean[] grid) {
+		return grid;
 	}
 }
 
