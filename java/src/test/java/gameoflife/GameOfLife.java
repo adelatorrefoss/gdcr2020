f304a11a20f5e23def32098d1e66e8322f42e87a
Antonio de la Torre <antonio@codesai.com>
2020-11-07 11:03:45 +0100

    First test

--- java/src/test/java/gameoflife/GameOfLife.java
+++ java/src/test/java/gameoflife/GameOfLife.java
@@ -2,13 +2,45 @@ package gameoflife;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class GameOfLife {
 
+	public boolean[][] grid;
+
 	@Test
-	public void fix_me_and_rename() {
-		assertFalse(true);
+	public void everything_is_dead_remains_dead() {
+		initGrid();
+
+		assertEquals(false, grid[0][0]);
+		assertEquals(false, grid[0][1]);
+		assertEquals(false, grid[0][2]);
+		assertEquals(false, grid[1][0]);
+		assertEquals(false, grid[1][1]);
+		assertEquals(false, grid[1][2]);
+		assertEquals(false, grid[2][0]);
+		assertEquals(false, grid[2][1]);
+		assertEquals(false, grid[2][2]);
 	}
 
+	@Test
+	public void a_cell_who_has_less_than_two_neighbours() {
+		initGrid();
+
+
+	}
+
+	private void initGrid() {
+		grid = new boolean[3][3];
+		grid[0][0] = false;
+		grid[0][1] = false;
+		grid[0][2] = false;
+		grid[1][0] = false;
+		grid[1][1] = false;
+		grid[1][2] = false;
+		grid[2][0] = false;
+		grid[2][1] = false;
+		grid[2][2] = false;
+	}
 }
