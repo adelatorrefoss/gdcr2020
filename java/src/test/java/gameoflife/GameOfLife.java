4cf8d83f0fb4af0a5e4b05dd44e812f5a776e0f1
Antonio de la Torre <antonio@codesai.com>
2020-11-07 10:33:14 +0100

    init java

---
+++ java/src/test/java/gameoflife/GameOfLife.java
@@ -0,0 +1,14 @@
+package gameoflife;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class GameOfLife {
+
+	@Test
+	public void fix_me_and_rename() {
+		assertFalse(true);
+	}
+
+}
